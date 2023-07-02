c9efe53b8be55e8af997b4bf8add7d1652051e72
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-07-02 17:29:10 -0500

    [nc] base64 was injecting newlines, added -w 0 to prevent
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/write_key_values_to_op.sh
+++ scripts/write_key_values_to_op.sh
@@ -8,7 +8,7 @@ for key in $(jq -r 'keys_unsorted[]' $GLOBAL_VALUES); do
     fields=$(jq -r --arg key $key '.[$key]' $GLOBAL_VALUES)
     for field in $(echo $fields | jq -r 'keys_unsorted[]'); do
         echo "write $key/$field"
-        value=$(echo $fields | jq -r --arg field $field '.[$field]' | base64)
+        value=$(echo $fields | jq -r --arg field $field '.[$field]' | base64 -w 0)
         op item edit $key --vault $VAULT $field=$value >/dev/null
     done
 done
